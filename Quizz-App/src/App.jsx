a7e0a14db84f793d81b40b056352386ce5ffaf2e
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-10-04 10:07:23 +0100

    dealing with quiz logic

--- Quizz-App/src/App.jsx
+++ Quizz-App/src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
-  const [country, setCountry] = useState('')
-  const handleSubmit = (e) =>{
+  const [formInput, setFormInput] = useState('')
+  const [country, setCountry] = useState([])
+  const handleSubmit = async(e) =>{
     e.preventDefault();
-    setCountry('');
-    console.log(country);
+    setFormInput('');
+    console.log(formInput);
     
   }
+  useEffect(() =>{
+    const fetchCOuntries = async() =>{
+      const response = await axios.get('http://localhost:3000')
+      setCountry(response.data);
+      console.log(response.data);
+    }
+    fetchCOuntries();
+  }, [])
   return (
     <>
       <div className='generalContainer'>
-        <form action="POST" onSubmit={handleSubmit}>
-          <input onChange={(e) => setCountry(e.target.value)} value={country} />
-          <button type='submit'>Submit</button>
-        </form>
+        <div className='quizzContainer'>
+          <h1>{country.length > 0 ?
+            country[Math.floor(Math.random() * country.length)].country: <p>loading.....</p> }
+          </h1>
+          <form onSubmit={handleSubmit} className='formContainer'>
+            <input onChange={(e) => setFormInput(e.target.value)} value={formInput} />
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+        
       </div>
     </>
   )
--- World+Capital+Quiz/index.js
+++ World+Capital+Quiz/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
@@ -13,43 +14,53 @@ let quiz = [
 let totalCorrect = 0;
 
 // Middleware
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 let currentQuestion = {};
 
 // GET home page
-app.get("/", async (req, res) => {
-  totalCorrect = 0;
-  await nextQuestion();
-  console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
+app.get("/", (req, res) => {
+  res.send(quiz);
 });
-
-// POST a new post
 app.post("/submit", (req, res) => {
-  let answer = req.body.answer.trim();
-  let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-    totalCorrect++;
-    console.log(totalCorrect);
-    isCorrect = true;
-  }
-
-  nextQuestion();
-  res.render("index.ejs", {
-    question: currentQuestion,
-    wasCorrect: isCorrect,
-    totalScore: totalCorrect,
-  });
-});
-
-async function nextQuestion() {
-  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-
-  currentQuestion = randomCountry;
-}
-
+  const input = req.body.input.trim();
+})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+  // let currentQuestion = {};
+  // app.get("/", async (req, res) => {
+  //   totalCorrect = 0;
+  //   await nextQuestion();
+  //   console.log(currentQuestion);
+  //   res.send(quiz)
+  // });
+  
+  // // POST a new post
+  // app.post("/submit", (req, res) => {
+  //   let answer = req.body.answer.trim();
+  //   let isCorrect = false;
+  //   if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  //     totalCorrect++;
+  //     console.log(totalCorrect);
+  //     isCorrect = true;
+  //   }
+  
+  //   nextQuestion();
+  //   res.render("index.ejs", {
+  //     question: currentQuestion,
+  //     wasCorrect: isCorrect,
+  //     totalScore: totalCorrect,
+  //   });
+  // });
+  
+  // async function nextQuestion() {
+  //   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  
+  //   currentQuestion = randomCountry;
+  // }
+  
