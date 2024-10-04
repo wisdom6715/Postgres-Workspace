085c3be273d4bf98ff01b2f10cc7e7ce88728263
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-10-04 11:04:17 +0100

    sample working now

--- Quizz-App/src/App.jsx
+++ Quizz-App/src/App.jsx
@@ -1,28 +1,40 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
 function App() {
   const [formInput, setFormInput] = useState('')
+  const score = useRef(1)
   const [selectedCountry, setSelectedCountry] = useState(null)  // Random country for display
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setFormInput('');
     console.log(formInput);
+    if(formInput.toLowerCase() === selectedCountry.toLowerCase()){
+      const currentScore = 5 * (score.current++);
+      // i want to increment the current score by multiplication by 2
+      alert(currentScore, 'points!');
+      
+    } else{
+      alert('Game over');
+    }
   }
   const fetchCountries = async () => {
     const response = await axios.get('http://localhost:3000');
     // setCountries(response.data);
     const randomCountry = response.data[Math.floor(Math.random() * response.data.length)].country;
     setSelectedCountry(randomCountry)
-    // // Only select a random country once after the data is fetched
-    // if (response.data.length > 0 && !selectedCountry) {
-    //   const randomCountry = response.data[Math.floor(Math.random() * response.data.length)].country;
-    //   setSelectedCountry(randomCountry); // Set the selected random country once
-    // }
   }
-
+  useEffect(() =>{
+    const fetchCountries = async () => {
+      const response = await axios.get('http://localhost:3000');
+      // setCountries(response.data);
+      const randomCountry = response.data[Math.floor(Math.random() * response.data.length)].country;
+      setSelectedCountry(randomCountry)
+    }
+    fetchCountries();
+  }, [])
   return (
     <>
       <div className='generalContainer'>
@@ -30,7 +42,7 @@ function App() {
           {/* Display the selected country */}
           {selectedCountry ? selectedCountry : 'Loading...'}
           <form onSubmit={handleSubmit} className='formContainer'>
-            <input onChange={(e) => setFormInput(e.target.value)} value={formInput} />
+            <input onChange={(e) => setFormInput(e.target.value)} value={formInput} required/>
             <button type='submit' onClick={fetchCountries}>Submit</button>
           </form>
         </div>
--- World+Capital+Quiz/index.js
+++ World+Capital+Quiz/index.js
@@ -11,56 +11,14 @@ let quiz = [
   { country: "United States of America", capital: "New York" },
 ];
 
-let totalCorrect = 0;
-
 // Middleware
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 
-let currentQuestion = {};
-
-// GET home page
 app.get("/", (req, res) => {
   res.send(quiz);
 });
-app.post("/submit", (req, res) => {
-  const input = req.body.input.trim();
-})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-
-  // let currentQuestion = {};
-  // app.get("/", async (req, res) => {
-  //   totalCorrect = 0;
-  //   await nextQuestion();
-  //   console.log(currentQuestion);
-  //   res.send(quiz)
-  // });
-  
-  // // POST a new post
-  // app.post("/submit", (req, res) => {
-  //   let answer = req.body.answer.trim();
-  //   let isCorrect = false;
-  //   if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-  //     totalCorrect++;
-  //     console.log(totalCorrect);
-  //     isCorrect = true;
-  //   }
-  
-  //   nextQuestion();
-  //   res.render("index.ejs", {
-  //     question: currentQuestion,
-  //     wasCorrect: isCorrect,
-  //     totalScore: totalCorrect,
-  //   });
-  // });
-  
-  // async function nextQuestion() {
-  //   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-  
-  //   currentQuestion = randomCountry;
-  // }
-  
