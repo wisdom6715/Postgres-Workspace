717e7e66279401d38f899a60ad1e9044f23f3927
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-10-04 10:39:11 +0100

    few errors fixed

--- Quizz-App/src/App.jsx
+++ Quizz-App/src/App.jsx
@@ -2,40 +2,41 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
-
 function App() {
   const [formInput, setFormInput] = useState('')
-  const [country, setCountry] = useState([])
-  const handleSubmit = async(e) =>{
+  const [selectedCountry, setSelectedCountry] = useState(null)  // Random country for display
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setFormInput('');
     console.log(formInput);
-    
   }
-  useEffect(() =>{
-    const fetchCOuntries = async() =>{
-      const response = await axios.get('http://localhost:3000')
-      setCountry(response.data);
-      console.log(response.data);
-    }
-    fetchCOuntries();
-  }, [])
+  const fetchCountries = async () => {
+    const response = await axios.get('http://localhost:3000');
+    // setCountries(response.data);
+    const randomCountry = response.data[Math.floor(Math.random() * response.data.length)].country;
+    setSelectedCountry(randomCountry)
+    // // Only select a random country once after the data is fetched
+    // if (response.data.length > 0 && !selectedCountry) {
+    //   const randomCountry = response.data[Math.floor(Math.random() * response.data.length)].country;
+    //   setSelectedCountry(randomCountry); // Set the selected random country once
+    // }
+  }
+
   return (
     <>
       <div className='generalContainer'>
         <div className='quizzContainer'>
-          <h1>{country.length > 0 ?
-            country[Math.floor(Math.random() * country.length)].country: <p>loading.....</p> }
-          </h1>
+          {/* Display the selected country */}
+          {selectedCountry ? selectedCountry : 'Loading...'}
           <form onSubmit={handleSubmit} className='formContainer'>
             <input onChange={(e) => setFormInput(e.target.value)} value={formInput} />
-            <button type='submit'>Submit</button>
+            <button type='submit' onClick={fetchCountries}>Submit</button>
           </form>
         </div>
-        
       </div>
     </>
   )
 }
 
-export default App
+export default App;
