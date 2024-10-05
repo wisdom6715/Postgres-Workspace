afb1f2293a4363bad6f64a20c8594e7dd1b7087f
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-10-05 23:45:35 +0100

    Postgres connected

--- World+Capital+Quiz/index.js
+++ World+Capital+Quiz/index.js
@@ -1,6 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors'
+import pg from 'pg';
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "wisdom6715",
+  port: 5432,
+})
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -10,6 +20,16 @@ let quiz = [
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+// connecting postgres
+db.query('SELECT * FROM capitals()', (err, res) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+  } else {
+    quiz = res.rows;
+    // console.log('Connected to the database:', res.rows);
+  }
+  db.end(); // close the connection
+});
 
 // Middleware
 app.use(cors())
